126705b5c4d8444459f04ba668f6f52f477ef3a8
KhurramSaeed700 <kssuper007@gmail.com>
2025-01-29 16:06:24 +0500

    made car gallery text link to home page

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -3,22 +3,30 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
+import Link from "next/link";
 
 // Navbar
 export function TopNav() {
   const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Car Gallery</div>
+      <Link href="/" className="flex items-center gap-4">
+        <div>Car Gallery</div>
+      </Link>
       <div className="flex items-center gap-4">
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UploadButton endpoint="imageUploader" onClientUploadComplete={()=>{router.refresh()}} />
-              <UserButton />
-            </SignedIn>
-          </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
+          <UserButton />
+        </SignedIn>
+      </div>
     </nav>
   );
 }
