c320e99161f2ef39c46331106f45ebcfe4f2a2e5
KhurramSaeed700 <kssuper007@gmail.com>
2025-01-29 16:45:02 +0500

    adjusted hompage image ui look

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
 import { getMyImages } from "~/server/queries";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -12,21 +13,23 @@ async function Images() {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
+    <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
       {images.map((image) => (
         <div key={image.id} className="flex flex-col items-center">
-          <div className="relative h-48 w-full">
-            <Image
-              src={image.url}
-              width={480}
-              height={480}
-              alt={image.name}
-              style={{ objectFit: "contain" }}
-              loading="lazy" // loading the image until it reaches a calculated distance from the viewport.
-              className="rounded-md shadow-md"
-            />
+          <div className="relative h-48 w-full max-w-xs ">
+            <Link href={`/img/${image.id}`}>
+              <Image
+                src={image.url}
+                width={480}
+                height={480}
+                alt={image.name}
+                style={{ objectFit: "cover" }}
+                loading="lazy" // loading the image until it reaches a calculated distance from the viewport.
+                className="rounded-md shadow-md w-full h-full"
+              />
+            </Link>
           </div>
-          <div className="text-center text-sm">{image.name}</div>
+          <div className=" text-sm mt-2 max-w-full text-left">{image.name}</div>
         </div>
       ))}
     </div>
@@ -35,7 +38,7 @@ async function Images() {
 
 export default async function HomePage() {
   return (
-    <main className="pl-3 pr-3">
+    <main className="px-3">
       <SignedOut>
         <div className="text-center text-lg">Sign in to view your images</div>
       </SignedOut>
