25019bed5454fa8c395130fe41bfd8bdffd6ee69
KhurramSaeed700 <kssuper007@gmail.com>
2025-02-03 00:20:40 +0500

    fixed clerk client outdated code

--- src/app/components/full-img-page.tsx
+++ src/app/components/full-img-page.tsx
@@ -1,20 +1,30 @@
 import { getImageById } from "~/server/queries";
 import { notFound } from "next/navigation";
-import Image from "next/image";
-import { Suspense } from "react";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function FullPageImageView(props: { id: number }) {
   try {
     const image = await getImageById(props.id);
     if (!image) notFound();
 
+    // changed syntax for clerkclient
+    // https://www.reddit.com/r/nextjs/comments/1gpczqc/clerk_next_js_issue/?rdt=40145
+    const clerk = await clerkClient();
+    const uploader = await clerk.users.getUser(image.userId);
+
     return (
       <div className="flex h-full w-full">
-        <div className="flex flex-shrink items-center justify-center">
+        <div className="flex flex-shrink items-center justify-center border-r-2 pr-3">
           <img src={image.url} className="w-96 object-contain" />
         </div>
         <div className="flex w-48 flex-col">
-          <div className="pl-5 text-xl font-bold text-white">{image.name}</div>
+          <div className="border-b-2 pb-2 pl-5 text-lg font-bold text-white">
+            {image.name}
+          </div>
+          <div className="flex flex-col">
+            <span>Uploaded By:</span>
+            <span>{uploader.fullName}</span>
+          </div>
         </div>
       </div>
     );
